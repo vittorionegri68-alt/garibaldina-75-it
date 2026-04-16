a535880e63dd6792d030d6479a354fb11e106de8
vittorionegri68-alt <vittorionegri68@gmail.com>
2026-04-16 14:11:28 +0200

    Update Instagram handle in contact info

--- src/config/contact_info.js
+++ src/config/contact_info.js
@@ -2,7 +2,7 @@ export const contactInfo = {
   phone: '+39 392 520 0650',
   email: 'luceacollection@gmail.com',
   address: 'Corso C.B. Cavour 75, 47521 Cesena FC, Italia',
-  instagram: 'https://www.instagram.com/luceacollection',
+  instagram: 'https://www.instagram.com/luceacollection_',
   web_it: 'https://romagna-affitti-brevi.it/',
   web_en: 'https://romagna-short-stay.com/',
 };
