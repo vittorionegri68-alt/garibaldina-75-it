2cbb66a54b977184c093f7e330d4b5d99cae2dd1
vittorionegri68-alt <vittorionegri68@gmail.com>
2026-04-28 08:26:01 +0200

    Update URLs for Italian and English language sites

--- src/config/site_config.js
+++ src/config/site_config.js
@@ -2,8 +2,8 @@ export const config = {
   airbnbUrl: 'https://www.airbnb.it/rooms/1558562623962692060',
   propertyName: 'Garibaldina 75',
   location: 'Cesena, Emilia-Romagna',
-  langIT: 'https://Garibaldina-75.vercel.app',
-  langEN: 'https://Garibaldina-75-en.vercel.app',
+  langIT: 'https://garibaldina-75.romagna-affitti-brevi.it',
+  langEN: 'https://garibaldina-75.romagna-short-stay.com',
   currentLang: 'IT',
   googleAnalyticsId: 'G-TEST123',
 };
